6cecabcbe53009f0419fa20490cac9867fcd617c
Mauricio Scheffer <mauricioscheffer@gmail.com>
2018-06-14 10:40:41 +0100

    Add project files.

---
+++ metabase-exporter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace metabase_exporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
