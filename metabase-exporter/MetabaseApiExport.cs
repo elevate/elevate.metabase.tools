1d67c00b01b3a169db06c81fef9b4fb380d2b965
Mauricio Scheffer <mauricioscheffer@gmail.com>
2018-06-15 17:49:18 +0100

    Move Metabase API export/import functions to their own files

---
+++ metabase-exporter/MetabaseApiExport.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metabase_exporter
+{
+    public static class MetabaseApiExport
+    {
+        public static async Task<string> Export(this MetabaseApi api)
+        {
+            var cards = await api.GetAllCards();
+            var nonArchivedCards = cards.Where(x => x.Archived == false).ToArray();
+
+            var dashboards = await api.GetAllDashboards();
+            var nonArchivedDashboards = dashboards.Where(x => x.Archived == false).ToArray();
+
+            var collections = await api.GetAllCollections();
+            var nonArchivedCollections = collections.Where(x => x.Archived == false).ToArray();
+
+            var state = new MetabaseState
+            {
+                Cards = nonArchivedCards,
+                Dashboards = nonArchivedDashboards,
+                Collections = nonArchivedCollections,
+            };
+
+            return JsonConvert.SerializeObject(state, Formatting.Indented);
+        }
+
+
+    }
+}
