14c0e66b12af82ac082344f3698e40e880fed4f5
Mauricio Scheffer <mauricioscheffer@gmail.com>
2024-05-04 17:32:30 +0100

    Upgrade to .net 8.0

--- shell.nix
+++ shell.nix
@@ -1,9 +1,9 @@
 with (import (builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/808c0d8c53c7ae50f82aca8e7df263225cf235bf.tar.gz";
-  sha256 = "1kgk5jqc93kr180r6k32q1n0l9xk8vwji72i1zc2ijja61cgdvmh";
+  url = "https://github.com/NixOS/nixpkgs/archive/5fd8536a9a5932d4ae8de52b7dc08d92041237fc.tar.gz";
+  sha256 = "0hyfifrhzxsdjj80sh5fpwcgm6zq5vx6ilh0lvp2dw6fzay1vrd0";
 })){};
 mkShell {
     buildInputs = [ 
-      dotnet-sdk_6
+      dotnet-sdk_8
     ];
 }
