b8b399def2f3f348e9fe281938f1c701beb08967
Mauricio Scheffer <mauricioscheffer@gmail.com>
2024-07-18 12:21:31 +0100

    Flake description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Description for the project";
+  description = "Export/Import Metabase questions/dashboard/collections";
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
